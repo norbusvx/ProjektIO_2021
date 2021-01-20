f714dab325a9665f6446be50355e2cdf9d8cf4c6
Natalia Koc <natalia-koc@o2.pl>
2021-01-20 13:40:29 +0100

    zmiana portu na 8081
    Zmieniłam port na 8081, bo u mnie port 8080 jest już czymś zajęty i nie mogę odpalić programu

--- ProjektIO/src/main/java/com/example/lab04/controlers/NataliaController.java
+++ ProjektIO/src/main/java/com/example/lab04/controlers/NataliaController.java
@@ -3,11 +3,17 @@ package com.example.lab04.controlers;
 
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/Natalia")
 public class NataliaController {
 
-
+    @GetMapping("/szegolyProduktu")
+    public String szegolyProduktu(Model model) {
+        model.addAttribute("produkt", DatabaseDumps.produkty.get(1));
+        return "szegolyProduktu";
+    }
 
 }
