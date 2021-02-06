661b924e2c66d8a6bae894bed3c8e0cb89c0c489
Norbert <norbusvx95@gmail.com>
2021-02-06 18:05:40 +0100

    Dodałem dostęp do podglądu h2

--- ProjektIO/src/main/java/com/example/lab04/config/SpringSecurityConfig.java
+++ ProjektIO/src/main/java/com/example/lab04/config/SpringSecurityConfig.java
@@ -50,6 +50,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .and().csrf().ignoringAntMatchers("/h2-console/**")
+                .and().headers().frameOptions().sameOrigin();
+        //to u góry włącza dostęp do podglądu h2
+        http
 
                 .authorizeRequests()
                 .antMatchers("/n/wyslijPrzelew/**").hasRole("USER")
